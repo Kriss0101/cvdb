903bdd2c050b6f5e41f0bccbfa18b0256fecc11a
Kristofer Stenberg <stenberg.kristofer@gmail.com>
2018-01-17 08:53:34 +0100

    Created a Spring Boot project

---
+++ src/main/java/kriss0101/cvdb/cvdb/CvdbApplication.java
@@ -0,0 +1,12 @@
+package kriss0101.cvdb.cvdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvdbApplication.class, args);
+	}
+}
